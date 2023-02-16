bfca0a28a8fe6bfe537ce5f820d6bb7ec16d800e
Emmanuel Andy Eze <emmanuel.amdyeze@gmail.com>
2023-02-16 15:27:59 +0100

    fixed login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,9 +9,9 @@ export const authOptions = {
 		}),
 		// ...add more providers here
 	],
-	pages: {
-		signIn: '/auth/signin',
-	},
+	// pages: {
+	// 	signIn: '/auth/signin',
+	// },
 	callbacks: {
 		async session({ session, token, user }) {
 			session.user.username = session.user.name
